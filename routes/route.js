44014278a9a2f573ced86ea6fea78b0f4ad37bbf
Noureen Fatima <karissa.kaal@gmail.com>
2023-05-18 03:16:45 +0500

    Transaction

--- routes/route.js
+++ routes/route.js
@@ -9,9 +9,12 @@ const addBankUser = require('../controllers/addBankUser');
 const updateCurrentUser = require('../controllers/updateCurrentUser');
 const trans = require('../controllers/addTransaction');
 const gettrans = require('../controllers/getAllTransactions');
+const gettransdata = require('../controllers/getTransactions');
 
 const reg = require("../controllers/getRegUsers");
 const currGet = require("../controllers/getCurrentUser");
+const { getUserDataByID } = require('../controllers/userController');
+
 
 router.post('/signupverify', signUpVerify);
 router.post('/register', register);
@@ -25,5 +28,8 @@ router.get('/CurrentUser', currGet);
 router.post('/transaction', trans);
 router.get('/transaction', gettrans);
 
+router.get('/user/:id', getUserDataByID);
+
+router.get('/transactionData', gettransdata);
 module.exports = router;
 
